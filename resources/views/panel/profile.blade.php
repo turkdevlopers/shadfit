786de7cf2ba2f46e6cb6be0654c71a179ee5bffb
Hossein Ghafouri <81298314+Hosseinghafouri@users.noreply.github.com>
2022-02-23 21:04:44 +0330

    Add profile page and edit botSetting page

--- resources/views/panel/profile.blade.php
+++ resources/views/panel/profile.blade.php
@@ -3,38 +3,97 @@
     پروفایل
 @endsection
 @section('content')
-<div class="container-fuild pt-4 px-4">
-    <div class="bg-light rounded-top p-5 shadow-sm">
-        <div class="bg-white p-4 shadow-sm">
-            <form action="" method="post">
-            <div class="container">
-                <div class="avatar-upload">
-                    <div class="avatar-edit">
+    <div class="container-fuild pt-4 px-4">
+        <div class="bg-light rounded-top p-5 shadow-sm">
+            <div class="bg-white p-4">
+                <form action="" method="post" class="form-d" id="form">
+                    <div class="container">
+                        <div class="avatar-upload">
+                            <div class="avatar-edit">
 
-                        <input type='file' id="imageUpload" accept=".png, .jpg, .jpeg" />
-                        <label for="imageUpload"><div class="icon-camera"><i class="fa fa-camera h4" aria-hidden="true"></i></div></label>
-                    </div>
-                    <div class="avatar-preview">
-                        <div id="imagePreview" style="background-image: url('{{ asset('panel/img/school2.jpeg') }}');">
+                                <input type='file' id="imageUpload" accept=".png, .jpg, .jpeg" />
+                                <label for="imageUpload" class="shadow-sm">
+                                    <div class="icon-camera"><i class="fa fa-camera h4" aria-hidden="true"></i></div>
+                                </label>
+                            </div>
+                            <div class="avatar-preview">
+                                <div id="imagePreview"
+                                    style="background-image: url('{{ asset('panel/img/school-icon.png') }}');">
+                                </div>
+                            </div>
+                        <small class="form__label">لوگوی مدرسه</small>
                         </div>
                     </div>
-                </div>
+
+                    <div class="bg-light rounded-top p-4">
+                        <div class="row">
+                            <div class="col-12 col-sm-12 text-center text-sm-start">
+                                <h3 class="text-center text-primary">ویرایش حساب</h3>
+
+                                <div class="part1 p-5">
+                                    <div class="d-flex justify-content-between col-lg-12 taraz">
+                                        <div class="control-form form-group d-block col-lg-5 col-md-12 col-sm-10 col-7">
+                                            <h5 class="d-flex edit-color">نام مدرسه: </h5>
+                                            <input type="text" class="form-control shadow-sm border-0" id="schoolName"
+                                                placeholder="نام مدرسه را وارد کنید" aria-label="Sizing example input"
+                                                aria-describedby="inputGroup-sizing-sm" minlength="5">
+                                            <small class="errortxt float-end mt-2"></small>
+                                        </div>
+                                        <div class="control-form form-group d-block col-lg-5 col-md-12 col-sm-10 col-7">
+                                            <h5 class="d-flex edit-color">آدرس مدرسه: </h5>
+                                            <input type="text" class="form-control shadow-sm border-0" id="address"
+                                                placeholder="آدرس مدرسه را وارد کنید" aria-label="Sizing example input"
+                                                aria-describedby="inputGroup-sizing-sm">
+                                            <small class="errortxt float-end mt-2"></small>
+                                        </div>
+                                    </div>
+                                    <br>
+                                    <div class="d-flex justify-content-between col-lg-12 taraz">
+                                        <div class="control-form form-group d-block col-lg-5 col-md-6 col-sm-10 col-7">
+                                            <h5 class="d-flex edit-color">شماره تلفن مدرسه: </h5>
+                                            <input type="tel" class="form-control shadow-sm border-0" id="numSchool"
+                                                aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                                                placeholder="شماره را وارد کنید"
+                                                pattern="(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}" />
+                                            <small class="errortxt float-end mt-2"></small>
+                                        </div>
+                                        <div class="control-form form-group d-block col-lg-5 col-md-6 col-sm-10 col-7">
+                                            <h5 class="d-flex edit-color">شماره تلفن مدیر مدرسه :</h5>
+                                            <input type="tel" class="form-control shadow-sm border-0 " id="numPerson"
+                                                aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                                                placeholder="شماره را وارد کنید"
+                                                pattern="(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}" />
+                                            <small class="errortxt float-end mt-2"></small>
+                                        </div>
+                                    </div>
+                                    <br>
+                                    <div class="d-flex justify-content-between col-lg-12 taraz">
+                                        <div class="control-form form-group d-block col-lg-5 col-md-6 col-sm-10 col-7">
+                                            <h5 class="d-flex edit-color">رمز عبور :</h5>
+                                            <input type="password" class="form-control shadow-sm border-0" id="password"
+                                                aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                                                placeholder="رمز عبور" min="0" />
+                                            <small class="errortxt float-end mt-2"></small>
+                                        </div>
+                                        <div class="control-form form-block d-block col-lg-5 col-md-6 col-sm-10 col-7">
+                                            <h5 class="d-flex edit-color">تکرار رمز عبور: </h5>
+                                            <input type="password" class="form-control shadow-sm border-0" id="password2"
+                                                placeholder="تکرار رمز عبور" aria-label="Sizing example input"
+                                                aria-describedby="inputGroup-sizing-sm">
+                                            <small class="errortxt float-end mt-2"></small>
+                                        </div>
+                                    </div>
+                                    <br>
+                                </div>
+                                <div class="text-center">
+                                 <button class="btn btn-primary m-2 shadow-sm col-lg-2">ثبت تغییرات</button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </form>
             </div>
-        </form>
-
-        <div class="bg-light rounded-top p-4 shadow-sm">
-            <div class="row">
-                <div class="col-12 col-sm-12 text-center text-sm-start">
-                    
-                </div>
-                <div class="col-12 col-sm-6 text-center text-sm-end">
-                    <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-
-                </div>
-            </div>
-        </div>
-
         </div>
     </div>
-</div>
 @endsection
